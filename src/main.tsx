888a18e50b594748575bde2c849531bfffebecd5
Hitury <xxhituryxx@gmail.com>
2025-09-09 12:24:33 +0200

    Theme-Switch

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,14 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
+import { HeroUIProvider } from "@heroui/system";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider>
+      <HeroUIProvider className="coffee">
         <App />
-      </Provider>
+      </HeroUIProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
