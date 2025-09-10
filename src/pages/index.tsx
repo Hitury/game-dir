a35685e2e5e761b987c55dbb4673ec2abb2104bc
Hitury <xxhituryxx@gmail.com>
2025-09-10 09:31:08 +0200

    Hamon overdriveeeeeeeeeeee

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,13 +14,13 @@ export default function IndexPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           {/* <span className={title()}>Make&nbsp;</span> */}
-          <span className={title({ color: "violet" })}>Anorix&nbsp;</span>
+          <span className={title({ color: "yellow" })}>Anorixia&nbsp;</span>
           <br />
           {/* <span className={title()}>
             websites regardless of your design experience.
           </span> */}
           <div className={subtitle({ class: "mt-4" })}>
-            Beautiful, fast and modern React UI library.
+            The Game Library for discovering, organizing, and sharing your favorite games.
           </div>
         </div>
 
@@ -49,8 +49,9 @@ export default function IndexPage() {
         <div className="mt-8">
           <Snippet hideCopyButton hideSymbol variant="bordered">
             <span>
-              Get started by editing{" "}
-              <Code color="primary">pages/index.tsx</Code>
+              <Code color="primary">
+              Get started by choosing{" "}
+              an option above!</Code>
             </span>
           </Snippet>
         </div>
