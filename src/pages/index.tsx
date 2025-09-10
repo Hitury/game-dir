24e9e6b89952b64e95ce8ef28a9713154b2993ce
ijvadeli <idhvandelindt@icloud.com>
2025-09-10 09:33:35 +0200

    ZA WARUDO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ export default function IndexPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           {/* <span className={title()}>Make&nbsp;</span> */}
-          <span className={title({ color: "yellow" })}>Anorixia&nbsp;</span>
+          <span className={title({ color: "yellow" })}>Placeholder&nbsp;</span>
           <br />
           {/* <span className={title()}>
             websites regardless of your design experience.
