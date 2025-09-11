5e4eea30da162547ccd747e50e679d47772fa7d5
ijvadeli <idhvandelindt@icloud.com>
2025-09-11 10:40:04 +0200

    push

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,20 +29,12 @@ export default function IndexPage() {
             isExternal
             className={buttonStyles({
               color: "primary",
-              radius: "full",
+              radius: "md",
               variant: "shadow",
             })}
             href={siteConfig.links.docs}
           >
-            Documentation
-          </Link>
-          <Link
-            isExternal
-            className={buttonStyles({ variant: "bordered", radius: "full" })}
-            href={siteConfig.links.github}
-          >
-            <GithubIcon size={20} />
-            GitHub
+            Explore
           </Link>
         </div>
 
