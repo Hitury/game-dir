07057cb559108d9a6540b67d71f4151f23e55b9c
ijvadeli <idhvandelindt@icloud.com>
2025-09-11 10:43:09 +0200

    sunbros

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import { Link } from "@heroui/link";
-import { Snippet } from "@heroui/snippet";
-import { Code } from "@heroui/code";
 import { button as buttonStyles } from "@heroui/theme";
 
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
 import DefaultLayout from "@/layouts/default";
 
 export default function IndexPage() {
@@ -36,16 +33,6 @@ export default function IndexPage() {
           >
             Explore
           </Link>
-        </div>
-
-        <div className="mt-8">
-          <Snippet hideCopyButton hideSymbol variant="bordered">
-            <span>
-              <Code color="primary">
-              Get started by choosing{" "}
-              an option above!</Code>
-            </span>
-          </Snippet>
         </div>
       </section>
     </DefaultLayout>
