d38cfb5ee7a74c558a0615b005e2255772baa647
ijvadeli <idhvandelindt@icloud.com>
2025-09-15 14:16:16 +0200

    Profiles

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,16 +4,23 @@ import DefaultLayout from "@/layouts/default";
 export default function DocsPage() {
   return (
     <DefaultLayout>
-      <section className="min-h-screen gap-4 py-8 md:py-10">
-        <div className="flex flex-row justify-center items-center">
-         <div className="text-center w-250 h-130">
-            <img className="w-[175px] rounded-full" src="/pfpIJvadeli.png"/>
-            <h1 className={title()}>IJvadeli</h1>
-          </div>
-          <div className="text-center w-250 h-130">
-            <img className="w-[175px] rounded-full" src="/pfpHitury.png"/>
-            <h1 className={title()}>Hitury</h1>
-          </div>
+      <section className="min-h-screen flex flex-nowrap justify-center items-start gap-8 px-4 py-12 overflow-x-auto">
+        {/* Card 1 */}
+        <div className="w-[400px] flex-shrink-0 flex flex-col items-center text-center">
+          <img className="w-[175px] rounded-full" src="/pfpIJvadeli.png" />
+          <h1 className={title()}>IJvadeli</h1>
+          <p className="text-left mt-3">
+            Hello, my name is IJsbrand van de Lindt. I'm 33 years old. My house is in the northeast section of Morioh...
+          </p>
+        </div>
+
+        {/* Card 2 */}
+        <div className="w-[400px] flex-shrink-0 flex flex-col items-center text-center">
+          <img className="w-[175px] rounded-full" src="/pfpHitury.png" />
+          <h1 className={title()}>Hitury</h1>
+          <p className="text-left mt-3">
+            You’re welcome to join our Discord server at any time! Also don’t forget to support us for only 5 dollars...
+          </p>
         </div>
       </section>
     </DefaultLayout>
