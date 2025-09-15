f2424d3bbc1bc20dbb987f4f0c8b7ca564c70e5e
ijvadeli <idhvandelindt@icloud.com>
2025-09-15 13:37:08 +0200

    -

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,4 +1,4 @@
-import { title } from "@/components/primitives";
+import { subtitle, title } from "@/components/primitives";
 import DefaultLayout from "@/layouts/default";
 
 export default function DocsPage() {
@@ -7,6 +7,7 @@ export default function DocsPage() {
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
         <div className="inline-block max-w-lg text-center justify-center">
           <h1 className={title()}>About</h1>
+          <h2 className={subtitle()}>Learn about the awesome developers who build this website</h2>
         </div>
       </section>
     </DefaultLayout>
