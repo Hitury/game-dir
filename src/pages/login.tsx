1d390a9c8245a590d66675b5c32b9e23665caa9d
Hitury <xxhituryxx@gmail.com>
2025-09-15 14:37:59 +0200

    Added peak to login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,8 +6,8 @@ export default function LoginPage() {
     <AuthLayout>
       <section className="h-[100%] flex items-center justify-center">
         <div className="flex flex-row bg-[#2d1f2c] rounded-sm p-7 rounded-lg shadow-lg">
-          <div className="bg-[#DB924B] w-110 h-130 rounded-sm flex items-start justify-center p-10">
-            <h1 className="font-semibold text-[42px] text-black">Login</h1>
+          <div className="bg-[#DB924B] w-110 h-130 rounded-sm flex items-start justify-center p-10 bg-[url('public/solaireandsiegmeyer.jpg')] bg-cover">
+            
           </div>
           <div className="bg-[#2d1f2c] w-110 h-130 flex items-start justify-center p-10 ml-7">
             <h1 className="font-semibold text-[40px] text-white">Create an Account</h1>
