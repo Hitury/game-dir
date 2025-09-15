afc3ff54219aef353ca69dffb1066baf422f2771
ijvadeli <idhvandelindt@icloud.com>
2025-09-15 13:46:24 +0200

    added pfp's

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -5,9 +5,17 @@ export default function DocsPage() {
   return (
     <DefaultLayout>
       <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-        <div className="inline-block max-w-lg text-center justify-center">
-          <h1 className={title()}>About</h1>
-          <h2 className={subtitle()}>Learn about the awesome developers who build this website</h2>
+        <div className="min-h-screen gap-4 py-8 md:py-10">
+          <div className="inline-block max-w-lg text-center justify-center">
+            <h1 className={title()}>About</h1>
+            <h2 className={subtitle()}>Learn about the awesome developers who build this website</h2>
+            <div className="bg-white w-110 h-130">
+              <img src="/pfpIJvadeli.png"/>
+            </div>
+            <div className="bg-black w-110 h-130">
+              <img src="/pfpHitury.png"/>
+            </div>
+          </div>
         </div>
       </section>
     </DefaultLayout>
