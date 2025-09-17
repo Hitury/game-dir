b2ea5cf6bb782b5b9550dc75f23a46b08ddbf6bd
ijvadeli <idhvandelindt@icloud.com>
2025-09-17 14:51:46 +0200

    Game Info Page Aangepast

--- src/pages/game-info.tsx
+++ src/pages/game-info.tsx
@@ -10,17 +10,17 @@ export default function DocsPage() {
           backgroundImage: 'url("/sunbros.jpg")',
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-          minHeight: '75%', // Optional: Set a minimum height
+          minHeight: '75%', // 75% good enough?
         }}
       >
         {/* Overlay */}
         <div
           className="absolute inset-0 bg-black opacity-50" // Dark overlay with 50% opacity
-          style={{ zIndex: 1 }} // Ensure it's above the background
+          style={{ zIndex: 1 }} // Make sure the overlay doesn't mess up the text
         ></div>
 
         {/* Content */}
-        <div className="relative z-10 inline-block max-w-lg text-center justify-center">
+        <div className="relative z-10 inline-block max-w-lg text-left align-bottom">
           <h1 className={title()}>Game Info</h1>
           <h2 className={subtitle()}>Creator: IJvadeli - 5 Stars 10/10 gameplay</h2>
           <h1 className={title()}>STER STER STER STER STER</h1> {/*5 sterren :thumbs_up:*/}
