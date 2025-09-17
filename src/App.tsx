a9f6fa7d851821ae128cde9d8555cf006b40f4f1
Hitury <xxhituryxx@gmail.com>
2025-09-17 10:07:39 +0200

    Wrapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
 import IndexPage from "@/pages/index";
 import ExplorerPage from "@/pages/explorer";
@@ -9,21 +10,43 @@ import AboutPage from "@/pages/about";
 import ContactPage from "@/pages/contact";
 import LoginPage from "@/pages/login";
 import SupportPage from "@/pages/support";
+import SignupPage from  "@/pages/signup";
+import PageWrapper from "@/components/PageWrapper"; // transition wrapper
 
 
 function App() {
   return (
-    <Routes>
-      <Route element={<IndexPage />} path="/" />
-      <Route element={<ExplorerPage />} path="/explorer" />
-      <Route element={<ProfilePage />} path="/profiles" />
-      <Route element={<GameInfo />} path="/game-info" />
-      <Route element={<ProfileUI />} path="/profile-ui" />
-      <Route element={<AboutPage />} path="/about" />
-      <Route element={<ContactPage />} path="/contact" />
-      <Route element={<LoginPage />} path="/login" />
-      <Route element={<SupportPage />} path="/support" />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        {/* Normal pages (no transitions) */}
+        <Route element={<IndexPage />} path="/" />
+        <Route element={<ExplorerPage />} path="/explorer" />
+        <Route element={<ProfilePage />} path="/profiles" />
+        <Route element={<GameInfo />} path="/game-info" />
+        <Route element={<ProfileUI />} path="/profile-ui" />
+        <Route element={<AboutPage />} path="/about" />
+        <Route element={<ContactPage />} path="/contact" />
+        <Route element={<SupportPage />} path="/support" />
+
+        {/* Auth pages with transitions */}
+        <Route
+          path="/login"
+          element={
+            <PageWrapper>
+              <LoginPage />
+            </PageWrapper>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PageWrapper>
+              <SignupPage />
+            </PageWrapper>
+          }
+        />
+      </Routes>
+    </AnimatePresence>
   );
 }
 
