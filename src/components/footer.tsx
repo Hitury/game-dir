e0282f228e1a0db95e976ae62062cc3f0f98f0fe
Hitury <xxhituryxx@gmail.com>
2025-09-22 12:37:49 +0200

    Margin top footer fix

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,7 +10,7 @@ const Year = new Date().getFullYear(); {/* Easy auto updating year function */ }
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-[#00000040] rounded-t-medium py-3">
+        <footer className="w-full bg-[#00000040] rounded-t-medium py-3 mt-15">
             <div className="footer-content text-center items-center flex flex-col">
                 <img src="/WhiteLogoCW.png" className="h-[75px]" />
                     <br/>
