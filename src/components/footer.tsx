04fcd499d15428ae06eb3ec7e44c1f162e0d4d20
ijvadeli <idhvandelindt@icloud.com>
2025-09-22 11:55:27 +0200

    Footer changed

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,24 +10,26 @@ const Year = new Date().getFullYear(); {/* Easy auto updating year function */ }
 
 export default function Footer() {
     return (
-        <footer className="w-full bg-[#00000050] rounded-t-medium py-3 mt-[100px]">
-            <div className="footer-content text-center">
-                <p className="font-bold text-inherit py-[10px]">🐦‍🔥 CINDERWATCH</p>
-                    <a href="/about" className="mx-[10px]">About</a>
-                    <a href="/support" className="mx-[10px]">Support</a>
-                    <a href="/contact" className="mx-[10px]">Contact</a>
+        <footer className="w-full bg-[#ffffff5f] rounded-t-medium py-3">
+            <div className="footer-content text-center items-center flex flex-col">
+                <img src="/CinderWatchLogo.png" className="h-[75px]" />
                     <br/>
-                    <div className="my-[5px] mt-[10px]">
-                    <Link isExternal href={siteConfig.links.twitter} title="Twitter">
-                        <TwitterIcon className="text-white mx-[5px]" />
-                    </Link>
-                    <Link isExternal href={siteConfig.links.discord} title="Discord">
-                        <DiscordIcon className="text-white mx-[5px]" />
-                    </Link>
-                    <Link isExternal href={siteConfig.links.github} title="GitHub">
-                        <GithubIcon className="text-white mx-[5px]" />
-                    </Link>
-                    </div>
+                        <div className="flex flex-row">
+                            <a href="/about" className="mx-[10px]">About</a>
+                            <a href="/support" className="mx-[10px]">Support</a>
+                            <a href="/contact" className="mx-[10px]">Contact</a>
+                        </div>
+                        <div className="my-[5px] mt-[10px]">
+                            <Link isExternal href={siteConfig.links.twitter} title="Twitter">
+                                <TwitterIcon className="text-[#c59f5e] mx-[5px]" />
+                            </Link>
+                            <Link isExternal href={siteConfig.links.discord} title="Discord">
+                                <DiscordIcon className="text-[#c59f5e] mx-[5px]" />
+                            </Link>
+                            <Link isExternal href={siteConfig.links.github} title="GitHub">
+                                <GithubIcon className="text-[#c59f5e] mx-[5px]" />
+                            </Link>
+                        </div>
                 <p className="">© {Year} Cinder Watch. All rights reserved.</p>
             </div>
         </footer>
