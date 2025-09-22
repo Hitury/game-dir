9e226df37a707f737ac606bec3d8038f6dd9c131
ijvadeli <idhvandelindt@icloud.com>
2025-09-22 14:27:55 +0200

    Changed IJvadeli ABOUT

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -15,16 +15,20 @@ export default function DocsPage() {
       <h1 className={title()}>Our Team:</h1>
       <section className="flex flex-nowrap justify-center items-start gap-8 px-4 py-12 overflow-x-auto [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
         {/* Card 1 */}
-        <div className="w-[50%] bg-[#00000050] p-[15px] h-[600px] flex-shrink-0 flex flex-col items-center text-center">
+        <div className="w-[50%] bg-[#00000050] p-[15px] h-[650px] flex-shrink-0 flex flex-col items-center text-center">
           <img className="w-[175px] rounded-full" src="/pfpIJvadeli.png" />
           <h1 className={title()}>IJvadeli</h1>
           <p className="text-left mt-3">
-            Hey! I am IJvadeli, the lead developer of Cinder Watch. I started this project because I wanted to create a platform that helps gamers stay informed about the latest news and updates in the gaming world. When I'm not coding, you can find me playing video games or exploring new technologies. Feel free to reach out if you have any questions or suggestions!
+            Hey! I am IJvadeli.<br/><br/>
+            I am a passionate developer and gamer, and I love creating cool projects like this website.
+            When I'm not coding, you can find me exploring new games or sharing my gaming experiences with friends.<br/><br/>
+            Skills:<br/>- JavaScript<br/>- TypeScript<br/>- React<br/>- Next.js<br/>- Node.js<br/>- HTML, CSS<br/>- Git<br/>- GitHub<br/>
+
           </p>
         </div>
 
         {/* Card 2 */}
-        <div className="w-[50%] bg-[#00000050] p-[15px] h-[600px] flex-shrink-0 flex flex-col items-center text-center">
+        <div className="w-[50%] bg-[#00000050] p-[15px] h-[650px] flex-shrink-0 flex flex-col items-center text-center">
           <img className="w-[175px] rounded-full" src="/pfpHitury.png" />
           <h1 className={title()}>Hitury</h1>
           <p className="text-left mt-3">
