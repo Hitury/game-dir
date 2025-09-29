57a8493d79b55732448a7a84fccd67052882f478
ijvadeli <idhvandelindt@icloud.com>
2025-09-29 09:56:09 +0200

    hasbrowncat

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,4 +1,5 @@
 import { Tabs, Tab, Card, CardBody } from "@heroui/react";
+import { subtitle  } from "./primitives";
 
 export default function TabFunc() {
   return (
@@ -20,8 +21,15 @@ export default function TabFunc() {
         <Tab key="details" title="Details">
           <Card className="bg-black/10 border border-[#DB924B]/20">
             <CardBody className="text-gray-400 text-sm">
-              Me Bomba — Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua...
+              <h2 className={subtitle()}>Details</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, nihil soluta repellat qui consectetur voluptatibus explicabo quos incidunt totam non sunt necessitatibus error vero voluptate animi, reprehenderit officia et blanditiis? Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus pariatur inventore, nihil eveniet consequuntur veniam modi iusto itaque sequi illo consequatur et eius, atque nesciunt ea! Vel quos odit impedit!
+                <br/><br/>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aut officiis dolorem facilis, magni voluptate deleniti eaque nulla rerum optio perspiciatis, tempore beatae accusantium quas doloribus nobis nemo adipisci. At. Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, ad. Excepturi architecto error vel rerum neque, ipsam distinctio dicta quasi numquam, incidunt quaerat quos dolore maxime sunt repudiandae temporibus natus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium, veniam. Nobis incidunt aperiam earum natus quos sequi, quaerat molestiae a quam nulla nostrum omnis?
+                <br/><br/>
+                Nostrum fugiat aut rem quaerat veniam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat impedit eveniet aliquam nemo mollitia ex consectetur, omnis dicta facilis aut nesciunt saepe dolor ullam sunt reprehenderit repellendus. Eum, error nobis! Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum saepe aliquam veniam ut rem fugiat temporibus! Consequatur non excepturi nobis necessitatibus suscipit, iure architecto optio corporis laudantium sit unde modi.
+                <img src="./public/hasbrowncat.gif" className="w-[100px]"/>
+              </p>
             </CardBody>
           </Card>
         </Tab>
@@ -29,8 +37,13 @@ export default function TabFunc() {
         <Tab key="reviews" title="Reviews">
           <Card className="bg-black/10 border border-[#DB924B]/20">
             <CardBody className="text-gray-400 text-sm">
-              DIOOOOO — Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-              tempor incididunt ut labore et dolore magna aliqua...
+              <h2 className={subtitle()}>Reviews</h2>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat modi ducimus asperiores iusto saepe corporis sed assumenda commodi, atque nam reiciendis aliquid voluptates placeat quae facere natus eligendi aut nesciunt. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio soluta quidem magnam doloribus consequuntur esse, iure facere. Perferendis omnis aliquam debitis, voluptate velit corrupti dolore obcaecati ea magnam, delectus est! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum quibusdam voluptate debitis nobis similique nam, mollitia animi tempora dolore illum culpa suscipit architecto, blanditiis vitae repellendus, molestias nihil at vel? Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque quidem aperiam quasi minus velit quas fugit minima nostrum explicabo mollitia ducimus id maxime dolor, illo officiis doloribus praesentium soluta at?
+              <br/><br/>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate tempore rerum ipsa ratione doloribus reiciendis? Veritatis id nobis reprehenderit eius quos voluptatum sed molestias sint iure, fuga eos saepe aperiam! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis totam rem quaerat provident velit expedita iure, voluptatum natus enim in eaque itaque recusandae, quos eos. Modi neque ea ipsam eum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum architecto possimus minima corporis magnam officia dolores cum minus, accusamus soluta sunt, odio officiis debitis neque ad quae facere deserunt cumque.
+              <br/><br/>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium culpa itaque, consequuntur provident ducimus qui consectetur! Quia sit quo enim possimus architecto tempore nisi voluptate, fuga officiis. Rerum, iusto asperiores.
+              <img src="./public/hasbrowncat.gif" className="w-[100px]"/>
             </CardBody>
           </Card>
         </Tab>
@@ -38,7 +51,13 @@ export default function TabFunc() {
         <Tab key="socials" title="Socials">
           <Card className="bg-black/10 border border-[#DB924B]/20">
             <CardBody className="text-gray-400 text-sm">
-              JoJo's Bizarre Adventure — Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+              <h2 className={subtitle()}>Socials</h2>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem vitae laudantium minus ipsa, illo fugiat saepe assumenda? Quisquam quam quasi, a eaque nesciunt quas exercitationem nemo debitis, aliquid commodi quidem. Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure voluptates aspernatur, magnam optio unde alias quam fugiat repudiandae cum modi, corporis qui atque eum aperiam doloremque? Quae perferendis quasi ex.
+              <br/><br/>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga consequatur maxime dignissimos atque maiores delectus reiciendis quaerat accusantium magni sed nulla in cum, est voluptas dolor! Autem rem blanditiis iste!
+              <br/><br/>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iure reiciendis architecto quia reprehenderit earum omnis impedit tempore, dicta quas nam sapiente cumque itaque nostrum aspernatur ipsam doloribus atque nulla laudantium.
+              <img src="./public/hasbrowncat.gif" className="w-[100px]"/>
             </CardBody>
           </Card>
         </Tab>
@@ -46,7 +65,8 @@ export default function TabFunc() {
         <Tab key="ouropinion" title="Our Opinion">
           <Card className="bg-black/10 border border-[#DB924B]/20">
             <CardBody className="text-gray-400 text-sm">
-              HAMON OVERDRIVEOOOOO — Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+              <h2 className={subtitle()}>Our Opinion</h2>
+              <img src="./public/hasbrowncat.gif" className="w-[100px]"/>
             </CardBody>
           </Card>
         </Tab>
@@ -54,7 +74,10 @@ export default function TabFunc() {
         <Tab key="pricing" title="Pricing">
           <Card className="bg-black/10 border border-[#DB924B]/20">
             <CardBody className="text-gray-400 text-sm">
-              Hallo Hitury — Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+              <h2 className={subtitle()}>Pricing</h2>
+              <p className="float-left">
+                <img src="./public/hasbrowncat.gif" className=" w-[18px] float-left"/>= 100$
+              </p>
             </CardBody>
           </Card>
         </Tab>
