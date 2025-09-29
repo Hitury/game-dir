cc2c15abbd3253423295402f72e52e9eed9c4c05
Hitury <xxhituryxx@gmail.com>
2025-09-29 14:26:54 +0200

    Light theme fix + authcontext implementation username

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -8,9 +8,9 @@ export default function DocsPage() {
     const { profile }= useProfile();
     return (
         <DefaultLayout>
-            <div className="w-[300px] m-auto text-center bg-[#000000] p-[15px] rounded-[15px] flex flex-col items-center">
-                <img src={profile?.avatar_url ? profile?.avatar_url : "/placeholder.jpg"} className="w-[75px] h-[75px] rounded-full border-2" />
-                <h1 className={subtitle()}>Welcome! *user*</h1>
+            <div className="w-[300px] m-auto text-center bg-black/20 p-[15px] rounded-[15px] flex flex-col items-center">
+                <img src={profile?.avatar_url ? profile?.avatar_url : "/placeholder.jpg"} className="w-[75px] rounded-full border-2 h-[75px]" />
+                <h1 className={subtitle()}>Welcome, {profile?.username}!</h1>
                 <Button className="my-[10px]">Choose Photo..</Button>
                 <Input className="my-[5px]" placeholder="Change Username.." />
                 <Input className="my-[5px]" placeholder="Change Password.." />
