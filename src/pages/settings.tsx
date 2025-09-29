ec541d2433066857873c226defe537dca250b04d
ijvadeli <idhvandelindt@icloud.com>
2025-09-29 13:47:28 +0200

    Changed the styling

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -2,6 +2,7 @@ import DefaultLayout from "@/layouts/default";
 import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 import { useProfile } from "@/hooks/useprofile"
+import { subtitle } from "@/components/primitives";
 
 export default function DocsPage() {
     const { profile }= useProfile();
@@ -9,8 +10,10 @@ export default function DocsPage() {
         <DefaultLayout>
             <div className="w-[300px] m-auto text-center bg-[#000000] p-[15px] rounded-[15px] flex flex-col items-center">
                 <img src={profile?.avatar_url ? profile?.avatar_url : "/placeholder.jpg"} className="w-[75px] rounded-full border-2" />
-                <Button className="my-[10px]">Upload..</Button>
-                <Input placeholder="Insert Username.."></Input>
+                <h1 className={subtitle()}>Welcome! *user*</h1>
+                <Button className="my-[10px]">Choose Photo..</Button>
+                <Input className="my-[10px]" placeholder="Change Username.."></Input>
+                <Input placeholder="Change Password.."></Input>
             </div>
         </DefaultLayout>
     );
