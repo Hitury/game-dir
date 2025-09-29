b411883aa4d501e280b36025a1fcaddc15e2692a
ijvadeli <idhvandelindt@icloud.com>
2025-09-29 13:36:58 +0200

    Changed settings and created a basic profile UI

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,9 +1,15 @@
 import DefaultLayout from "@/layouts/default";
+import { Button } from "@heroui/button";
+import { Input } from "@heroui/input";
 
 export default function DocsPage() {
     return (
         <DefaultLayout>
-            <p>Helo</p>
+            <div className="w-[300px] m-auto text-center bg-[#000000] p-[15px] rounded-[15px] flex flex-col items-center">
+                <img src="./public/placeholder.jpg" className="w-[75px] rounded-full border-2" />
+                <Button className="my-[10px]">Upload..</Button>
+                <Input placeholder="Insert Username.."></Input>
+            </div>
         </DefaultLayout>
     );
 }
